db1c3fde78b6eb909085f8ea658ccb84bf5b17aa
Errly223093 <lmjgmm@naver.com>
2024-02-27 23:35:27 +0800

    Installed React & Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="bg-red-400 h-screen">
+      <div className="font-bold">test</div>
+    </div>
+  );
+}
+
+export default App;
