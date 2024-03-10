6040bfab74f41b424d139e73eebd98dd1c9f8a00
Errly223093 <lmjgmm@naver.com>
2024-03-11 03:04:38 +0800

    chore : removed file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts}"],
   theme: {
-    extend: {
-      spacing: {
-        // w-360 h-800
-        360: "22.5rem",
-        800: "50rem",
-      },
-      backgroundImage: {
-        // bg-InGame
-        InGame: "url('../public/images/BackgroundImg/bgImg.png')",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
