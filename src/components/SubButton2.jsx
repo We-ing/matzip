518c714501a18673e3a4140ceb9ba0ea7e12e86b
Baekshook <ecgo0106@naver.com>
2024-03-10 23:21:37 +0900

    start/result page setting

---
+++ src/components/SubButton2.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./subButton2.css";
+
+export default function SubButton2() {
+  return (
+    <button
+      onClick={() => window.open("https://wepublic.com/c/matzip?tab=feed")}
+      className="button2"
+    >
+      DAO돌아가기
+    </button>
+  );
+}
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,15 +1,50 @@
-import React from "react";
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import "./animate.css";
+import StartButtons from "../conponents/StartButtons";
+import Loading from "../conponents/Loading";
 
-const Main = () => {
+export default function Main() {
+  const flavorCheck = "입맛\n첵췤";
+  const [loading, setLoading] = useState(false);
+
+  const handleStartButtonClick = () => {
+    // 버튼 클릭 시 로딩 시작
+    setLoading(true);
+  };
+
+  useEffect(() => {
+    // 로딩이 시작되면 2초 후에 로딩 종료 및 SecondPage로 이동
+    if (loading) {
+      const timeoutId = setTimeout(() => {
+        setLoading(false);
+      }, 2000);
+
+      // 컴포넌트가 언마운트될 때 타이머를 정리하여 메모리 누수 방지
+      return () => clearTimeout(timeoutId);
+    }
+  }, [loading]);
+
   return (
-    <div>
-      <Link to="/worldcup">
-        <button>시작버튼</button>
-      </Link>
+    <div className="bg-purple-300 min-h-screen flex flex-col justify-center items-center">
+      <div className="relative w-[360px] h-[800px] flex flex-col justify-center items-center">
+        <img
+          className="absolute"
+          src={`${process.env.PUBLIC_URL}/images/bg.png`}
+          alt="animals"
+        />
+        <div className="card mb-24">
+          <div className="bg">
+            <div className="font-bold text-7xl justify-center items-center flex ml-3 leading-tight tracking-[15px] whitespace-pre-wrap">
+              {flavorCheck}
+            </div>
+          </div>
+          <div className="blob"></div>
+        </div>
+        <button onClick={handleStartButtonClick}>
+          <StartButtons />
+        </button>
+      </div>
     </div>
   );
-};
-
-export default Main;
+}
