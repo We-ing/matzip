f957eda9a7d34f1da20375e6d4967668e59546bd
Baekshook <ecgo0106@naver.com>
2024-03-12 19:21:36 +0900

    refactor: delete Test.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,19 @@ import Main from "./pages/Main";
 import Worldcup from "./pages/Worldcup";
 import Loading from "./conponents/Loading";
 import Result from "./pages/Result";
-import Test from "./pages/Test";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Main />} />
-                <Route path="/test" element={<Test />} />
-                <Route path="/worldcup" element={<Worldcup />} />
-                <Route path="/loadings" element={<Loading />} />
-                <Route path="/results/:flavorId" element={<Result />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+
+        <Route path="/worldcup" element={<Worldcup />} />
+        <Route path="/loadings" element={<Loading />} />
+        <Route path="/results/:flavorId" element={<Result />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
